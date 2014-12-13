ebfd5ae211a0bea5732680aa3a0933a5aa74f3ed
David Mauro <email@dmauro.com>
2014-12-13 04:18:11 -0500

    Update with more Slice-like implementation

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 Swift WeakArray
 ========
-Version 0.2  
+Version 0.3  
 WeakArray offers a collection type that behaves like a Swift Array, but will not retain any of its objects.
 
 Released under the Apache License, version 2.0
 
 How to Install
 ==============
-This should only be used with the latest version of Xcode 6.
 
-1. Clone this repository.  
-2. Add WeakArray.xcodeproj to your main target.  
-3. Link the WeakArray framework in your main target.  
+1. Add WeakArray as a [submodule](http://git-scm.com/docs/git-submodule) with `git submodule add https://github.com/dmauro/WeakArray` (ideally forking and pointing to your fork's url)
+2. Open the `WeakArray` folder, and drag `WeakArray.xcodeproj` into the file navigator of your app project. **NOTE: The WeakArray project needs to be added somewhere under the target project or you won't be able to add it to your target dependencies.**
+3. Ensure that the deployment target of the WeakArray project matches that of the application target.
+4. In your target's "Build Phases" panel, add `WeakArray.framework` to the "Target Dependencies"
+5. Click on the `+` button at the top left of the panel and select "New Copy Files Phase". Rename this new phase to "Copy Frameworks", set the "Destination" to "Frameworks"
+
+If that's too complicated, you can always just copy the files into your project manually and add them to your target. You'll only need the `WeakArray.h` and `WeakArray.swift` files.
+
 
 Usage
 =====
@@ -22,16 +26,16 @@ First make sure to import WeakArray:
 When creating a weak array, specify the collection type as you would with an Array:
 	
 	typealias myType = AnyObject
-	var a = WeakArray<MyType>()
+	var a: WeakArray<MyType> = []
 	
-You can also use array literals to create a WeakArray, but to do so you'll need to unwrap your optionals with ```!```:
+You can also use array literals to create a WeakArray, but if you specify any values you'll need to unwrap your optionals with ```!```:
 
 	var view: UIView? = UIView()
 	var a: WeakArray<UIView> = [view!]
 	
-Otherwise a WeakArray works just like an Array:
+Otherwise a WeakArray works much like an Array:
 
-	var a = WeakArray<AnyObject>()
+	var a: WeakArray<AnyObject> = []
 	
 	var view: UIView? = UIView()
 	
@@ -50,6 +54,9 @@ Otherwise a WeakArray works just like an Array:
 	// Remove at index
 	var view: UIView? = a.removeAtIndex(0)
 	
+	// Insert at index
+	a.insert(view, atIndex: 0)
+	
 	// isEmpty and count
 	if (!a.isEmpty) {
 		println("Count: \(a.count)")
@@ -57,15 +64,17 @@ Otherwise a WeakArray works just like an Array:
 	
 	// First and Last
 	var first: AnyObject? = a.first
-	var last: AnyObject? = a.list
+	var last: AnyObject? = a.last
 	
 	// Append WeakArray
-	var b = WeakArray<AnyObject>()
+	var b: WeakArray<AnyObject>() = []
 	a += b
 	
 	// Or append a standard library Array
-	var c = Array<AnyObject>()
+	var c: [AnyObject] = []
 	a += c
+
+Most of the methods you'll find in the Slice struct are implemented on WeakArray, so if there's an Array method you need, it is probably implemented. If you find a need for any of the few that are not implemented, let me know.
 
 You can also enumerate over a WeakArray like you would an Array, but keep in mind it will skip over nil values while count will include them, so **count may not
 match the number of enumerations you get**.
@@ -75,9 +84,9 @@ match the number of enumerations you get**.
         var obj1: UIView? = UIView()
         var obj2: UIView? = UIView()
         var obj3: UIView? = UIView()
-        a += obj1
-        a += obj2
-        a += obj3
+        a.append(obj1)
+        a.append(obj2)
+        a.append(obj3)
         obj2 = nil
         var i = 0     
         for value in a {
@@ -89,6 +98,12 @@ match the number of enumerations you get**.
     
 Version History
 ===============
+**v0.3**
+
+* Update for Swift 1.0. Thanks to [Mazyod](https://github.com/Mazyod) in [Pull Request #1](https://github.com/dmauro/WeakArray/pull/1).
+* Implemented more Slice methods.
+* Added equality operator overload.
+
 **v0.2**
 
 * Update to match beta 5 array changes
--- WeakArray/WeakArray.swift
+++ WeakArray/WeakArray.swift
@@ -8,6 +8,42 @@
 
 // MARK: Operator Overloads
 
+public func ==<T: Equatable>(lhs: WeakArray<T>, rhs: WeakArray<T>) -> Bool {
+    var areEqual = false
+    if lhs.count == rhs.count {
+        areEqual = true
+        for i in 0..<lhs.count {
+            if lhs[i] != rhs[i] {
+                areEqual = false
+                break
+            }
+        }
+    }
+    return areEqual
+}
+
+public func !=<T: Equatable>(lhs: WeakArray<T>, rhs: WeakArray<T>) -> Bool {
+    return !(lhs == rhs)
+}
+
+public func ==<T: Equatable>(lhs: Slice<T?>, rhs: Slice<T?>) -> Bool {
+    var areEqual = false
+    if lhs.count == rhs.count {
+        areEqual = true
+        for i in 0..<lhs.count {
+            if lhs[i] != rhs[i] {
+                areEqual = false
+                break
+            }
+        }
+    }
+    return areEqual
+}
+
+public func !=<T: Equatable>(lhs: Slice<T?>, rhs: Slice<T?>) -> Bool {
+    return !(lhs == rhs)
+}
+
 public func +=<T> (inout lhs: WeakArray<T>, rhs: WeakArray<T>) -> WeakArray<T> {
     lhs.items += rhs.items
     return lhs
@@ -79,29 +115,16 @@ public struct WeakArray<T: AnyObject>: SequenceType, Printable, DebugPrintable, 
         return GeneratorType(items: slice)
     }
 
-    mutating public func append(value: T?) {
-        let weak = Weak(value: value)
-        items.append(weak)
-    }
+    // MARK: - Slice-like Implementation
 
-    mutating public func removeAtIndex(i: Int) -> T? {
-        let weak = items.removeAtIndex(i)
-        return weak.value
-    }
-
-    mutating public func removeLast() -> T? {
-        let weak = items.removeLast()
-        return weak.value
-    }
-
-    public subscript(i: Int) -> T? {
+    public subscript(index: Int) -> T? {
         get {
-            let weak = items[i]
+            let weak = items[index]
             return weak.value
         }
         set(value) {
             let weak = Weak(value: value)
-            items[i] = weak
+            items[index] = weak
         }
     }
 
@@ -113,10 +136,72 @@ public struct WeakArray<T: AnyObject>: SequenceType, Printable, DebugPrintable, 
         }
         set(value) {
             items[range] = value.map {
-                (value: T?) -> Weak<T> in
+                (value: T?) -> WeakObject in
                 return Weak(value: value)
             }
         }
+    }
+
+    mutating public func append(value: T?) {
+        let weak = Weak(value: value)
+        items.append(weak)
+    }
+
+    mutating public func insert(newElement: T?, atIndex i: Int) {
+        let weak = Weak(value: newElement)
+        items.insert(weak, atIndex: i)
+    }
+
+    mutating public func removeAtIndex(index: Int) -> T? {
+        let weak = items.removeAtIndex(index)
+        return weak.value
+    }
+
+    mutating public func removeLast() -> T? {
+        let weak = items.removeLast()
+        return weak.value
+    }
+
+    mutating public func removeAll(keepCapacity: Bool) {
+        items.removeAll(keepCapacity: keepCapacity)
+    }
+
+    mutating public func removeRange(subRange: Range<Int>) {
+        items.removeRange(subRange)
+    }
+
+    mutating public func replaceRange(subRange: Range<Int>, with newElements: Slice<T?>) {
+        let weakElements = newElements.map { Weak(value: $0) }
+        items.replaceRange(subRange, with: weakElements)
+    }
+
+    mutating public func splice(newElements: Slice<T?>, atIndex i: Int) {
+        let weakElements = newElements.map { Weak(value: $0) }
+        items.splice(weakElements, atIndex: i)
+    }
+
+    mutating public func extend(newElements: Slice<T?>) {
+        let weakElements = newElements.map { Weak(value: $0) }
+        items.extend(weakElements)
+    }
+
+    public func filter(includeElement: (T?) -> Bool) -> WeakArray<T> {
+        var filtered: WeakArray<T> = []
+        for item in items {
+            if includeElement(item.value) {
+                filtered.append(item.value)
+            }
+        }
+        return filtered
+    }
+
+    public func reverse() -> WeakArray<T> {
+        var reversed: WeakArray<T> = []
+        let reversedItems = items.reverse()
+        for item in reversedItems {
+            reversed.append(item.value)
+        }
+        return reversed
     }
 }
 
